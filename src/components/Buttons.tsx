eae401702034cad61559ece15d171452b9e45b19
torvey <radek.smidoda@gmail.com>
2024-11-18 09:42:07 +0100

    feat: create generic toggle button

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,20 +1,43 @@
 import { FC } from "react";
-import { XMarkIcon } from "./icons";
+import { ChevronUpIcon, XMarkIcon } from "./icons";
 
 type ButtonProps = React.ComponentProps<"button">;
 
-export const ExpandButton: FC<ButtonProps> = ({ children, ...props }) => {
-  return (
-    <button className="hover:text-gray-700 transition-colors flex items-center justify-center" {...props}>
-      {children}
-    </button>
-  );
+type ToggleButtonProps = Omit<ButtonProps, "children"> & {
+    isExpanded: boolean;
+};
+
+export const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
+    return (
+        <button
+            className={`hover:text-gray-700 transition-colors items-center justify-center ${className}`}
+            {...props}
+        >
+            {children}
+        </button>
+    );
 };
 
 export const DeleteButton: FC<Omit<ButtonProps, "children">> = (props) => {
-  return (
-    <button className="hover:text-gray-700 transition-colors flex items-center justify-center" {...props}>
-      <XMarkIcon />
-    </button>
-  );
+    return (
+        <Button {...props}>
+            <XMarkIcon />
+        </Button>
+    );
+};
+
+export const ToggleButton: FC<ToggleButtonProps> = ({
+    isExpanded,
+    ...props
+}) => {
+    return (
+        <Button
+            {...props}
+            className={`transition-transform duration-300 ${
+                isExpanded ? "" : "rotate-180"
+            }`}
+        >
+            <ChevronUpIcon />
+        </Button>
+    );
 };
