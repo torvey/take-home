64469f8fadb98b27c0557b0bbed78b52c637e7b1
torvey <radek.smidoda@gmail.com>
2024-11-18 09:07:00 +0100

    feat: add expand and collapse functionality

--- src/components/Entrypoint.tsx
+++ src/components/Entrypoint.tsx
@@ -1,53 +1,75 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ListItem, useGetListData } from "../api/getListData";
 import { Card } from "./List";
 import { Spinner } from "./Spinner";
 
 export const Entrypoint = () => {
-  const [visibleCards, setVisibleCards] = useState<ListItem[]>([]);
-  const listQuery = useGetListData();
+    const [visibleCards, setVisibleCards] = useState<ListItem[]>([]);
+    const [expandedCardsIds, setExpandedCardsIds] = useState<number[]>([]);
 
-  // TOOD
-  // const deletedCards: DeletedListItem[] = [];
+    const listQuery = useGetListData();
 
-  useEffect(() => {
+    // TOOD
+    // const deletedCards: DeletedListItem[] = [];
+
+    const toggleExpand = useCallback((id: number) => {
+        setExpandedCardsIds((prev) => {
+            if (prev.includes(id)) {
+                return prev.filter((item) => item !== id);
+            }
+
+            return [...prev, id];
+        });
+    }, []);
+
+    useEffect(() => {
+        if (listQuery.isLoading) {
+            return;
+        }
+
+        setVisibleCards(listQuery.data?.filter((item) => item.isVisible) ?? []);
+    }, [listQuery.data, listQuery.isLoading]);
+
     if (listQuery.isLoading) {
-      return;
+        return <Spinner />;
     }
 
-    setVisibleCards(listQuery.data?.filter((item) => item.isVisible) ?? []);
-  }, [listQuery.data, listQuery.isLoading]);
-
-  if (listQuery.isLoading) {
-    return <Spinner />;
-  }
-
-  return (
-    <div className="flex gap-x-16">
-      <div className="w-full max-w-xl">
-        <h1 className="mb-1 font-medium text-lg">My Awesome List ({visibleCards.length})</h1>
-        <div className="flex flex-col gap-y-3">
-          {visibleCards.map((card) => (
-            <Card key={card.id} title={card.title} description={card.description} />
-          ))}
-        </div>
-      </div>
-      <div className="w-full max-w-xl">
-        <div className="flex items-center justify-between">
-          <h1 className="mb-1 font-medium text-lg">Deleted Cards (0)</h1>
-          <button
-            disabled
-            className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1"
-          >
-            Reveal
-          </button>
-        </div>
-        <div className="flex flex-col gap-y-3">
-          {/* {deletedCards.map((card) => (
+    return (
+        <div className="flex gap-x-16">
+            <div className="w-full max-w-xl">
+                <h1 className="mb-1 font-medium text-lg">
+                    My Awesome List ({visibleCards.length})
+                </h1>
+                <div className="flex flex-col gap-y-3">
+                    {visibleCards.map((card) => (
+                        <Card
+                            key={card.id}
+                            title={card.title}
+                            description={card.description}
+                            handleExpand={() => toggleExpand(card.id)}
+                            isExpanded={expandedCardsIds.includes(card.id)}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className="w-full max-w-xl">
+                <div className="flex items-center justify-between">
+                    <h1 className="mb-1 font-medium text-lg">
+                        Deleted Cards (0)
+                    </h1>
+                    <button
+                        disabled
+                        className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1"
+                    >
+                        Reveal
+                    </button>
+                </div>
+                <div className="flex flex-col gap-y-3">
+                    {/* {deletedCards.map((card) => (
             <Card key={card.id} card={card} />
           ))} */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,23 +4,41 @@ import { DeleteButton, ExpandButton } from "./Buttons";
 import { ChevronUpIcon } from "./icons";
 
 type CardProps = {
-  title: ListItem["title"];
-  description: ListItem["description"];
+    title: ListItem["title"];
+    description: ListItem["description"];
+    isExpanded?: boolean;
+    handleExpand?: () => void;
 };
 
-export const Card: FC<CardProps> = ({ title, description }) => {
-  return (
-    <div className="border border-black px-2 py-1.5">
-      <div className="flex justify-between mb-0.5">
-        <h1 className="font-medium">{title}</h1>
-        <div className="flex">
-          <ExpandButton>
-            <ChevronUpIcon />
-          </ExpandButton>
-          <DeleteButton />
+export const Card: FC<CardProps> = ({
+    title,
+    description,
+    isExpanded,
+    handleExpand,
+}) => {
+    return (
+        <div className="border border-black px-2 py-1.5">
+            <div className="flex justify-between mb-0.5">
+                <h1 className="font-medium">{title}</h1>
+                <div className="flex">
+                    <ExpandButton
+                        onClick={handleExpand}
+                        className={`transition-transform duration-300 ${
+                            isExpanded ? "" : "rotate-180"
+                        }`}
+                    >
+                        <ChevronUpIcon />
+                    </ExpandButton>
+                    <DeleteButton />
+                </div>
+            </div>
+            <p
+                className={`text-sm overflow-hidden transition-all duration-300 ${
+                    isExpanded ? "max-h-96" : "max-h-0"
+                }`}
+            >
+                {description}
+            </p>
         </div>
-      </div>
-      <p className="text-sm">{description}</p>
-    </div>
-  );
+    );
 };
